841b317c9c77dda8d204f395fd16a9a771f015df
Zapupa <vitaliy2005rus93@gmail.com>
2024-10-30 17:18:34 +0500

    Добавлена ViewModel для счётчика шагов

---
+++ app/src/main/java/com/example/selfconfidencefit/viewmodel/StepsViewModel.kt
@@ -0,0 +1,107 @@
+package com.example.selfconfidencefit.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.selfconfidencefit.data.local.models.StepsDay
+import com.example.selfconfidencefit.data.local.models.StepsGoal
+import com.example.selfconfidencefit.data.local.repository.StepsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StepsViewModel @Inject constructor(private val repository: StepsRepository) : ViewModel() {
+    fun readStepsDay(key: Long) : StepsDay? = repository.getStepsDay(key)
+
+    fun readAllStepsDays() : LiveData<List<StepsDay>> = repository.getAllStepsDays
+
+    fun readLatestStepsDay() : StepsDay = repository.getLatestStepsDay
+
+    fun readLatestStepsDayObservable() : LiveData<StepsDay> = repository.getLatestStepsDayObservable
+
+    fun insertStepsDay(steps: StepsDay, onSuccess: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertStepsDay(steps) {
+                viewModelScope.launch(Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
+
+    fun updateStepsDay(steps: StepsDay, onSuccess: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateStepsDay(steps) {
+                viewModelScope.launch(Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
+
+    fun addLatestSteps(stepsToAdd: Int, onSuccess: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addLatestSteps(stepsToAdd) {
+                viewModelScope.launch(Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
+
+    fun deleteAllStepsDaysButLatest(onSuccess: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteAllStepsDaysButLatest() {
+                viewModelScope.launch(Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
+
+    fun deleteAllStepsDaysButSeven(onSuccess: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteAllStepsDaysButSeven() {
+                viewModelScope.launch(Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
+
+    fun readGoal(key: Long): StepsGoal? = repository.getGoal(key)
+
+    fun readAllGoals(): LiveData<List<StepsGoal>> = repository.getAllGoals
+
+    fun insertGoal(goal: StepsGoal, onSuccess: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertGoal(goal) {
+                viewModelScope.launch(Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
+
+    fun updateGoal(goal: StepsGoal, onSuccess: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateGoal(goal) {
+                viewModelScope.launch(Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
+
+    fun deleteGoal(goal: StepsGoal, onSuccess: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteGoal(goal) {
+                viewModelScope.launch(Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
+}
